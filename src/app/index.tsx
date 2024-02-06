9729e50358453d89d9888f626852b4ed4596d824
gabriellopes232 <gabriel.lopes.nascimento.dev@gmail.com>
2024-02-06 20:21:58 -0300

    feat: create category list in home page

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,32 +1,44 @@
-import { CategoryButton } from '@/components/category-button'
-import { Header } from '@/components/header'
-import { View, FlatList, SectionList, Text } from 'react-native'
-import { CATEGORIES, MENU } from '@/utils/data/products'
-import { useState, useRef } from 'react'
-import { Product } from '@/components/product'
+import { useRef, useState } from 'react';
+import { View, FlatList, SectionList, Text } from 'react-native';
+import { Link } from 'expo-router';
+
+import { useCartStore } from '@/stores/cart-store';
+import { CATEGORIES, MENU, ProductProps } from '@/utils/data/products';
+
+import { Header } from '@/components/header';
+import { Product } from '@/components/product';
+import { CategoryButton } from '@/components/category-button';
 
 export default function Home() {
-  const [category, setCategory] = useState(CATEGORIES[0])
+  const cartStore = useCartStore();
+  const [category, setCategory] = useState(CATEGORIES[0]);
 
-  const sectionListRef = useRef<SectionList>(null)
+  const sectionListRef = useRef<SectionList<ProductProps>>(null);
+
+  const cartQuantityItems = cartStore.products.reduce(
+    (total, product) => total + product.quantity,
+    0,
+  );
 
   function handleCategorySelect(selectedCategory: string) {
-    setCategory(selectedCategory)
+    setCategory(selectedCategory);
 
-    const sectionIndex = CATEGORIES.findIndex((category) => category === selectedCategory)
+    const sectionIndex = CATEGORIES.findIndex(
+      (category) => category === selectedCategory,
+    );
 
     if (sectionListRef.current) {
       sectionListRef.current.scrollToLocation({
         animated: true,
         sectionIndex,
-        itemIndex: 0
-      })
+        itemIndex: 0,
+      });
     }
   }
 
   return (
-    <View className='flex-1 pt-8'>
-      <Header title='Faça o seu pedido' />
+    <View className="flex-1 pt-8">
+      <Header title="Faça seu pedido" cartQuantityItems={cartQuantityItems} />
 
       <FlatList
         data={CATEGORIES}
@@ -39,7 +51,7 @@ export default function Home() {
           />
         )}
         horizontal
-        className='max-h-10 mt-5'
+        className="max-h-10 mt-5"
         showsHorizontalScrollIndicator={false}
         contentContainerStyle={{ gap: 12, paddingHorizontal: 20 }}
       />
@@ -50,18 +62,17 @@ export default function Home() {
         keyExtractor={(item) => item.id}
         stickySectionHeadersEnabled={false}
         renderItem={({ item }) => (
-          <Product data={item} />
+          <Link href={`/product/${item.id}`} asChild>
+            <Product data={item} />
+          </Link>
         )}
-        renderSectionHeader={({ section: { title } }) =>
-          <Text className='text-xl text-white font-heading mt-8 mb-3'>
-            {title}
-          </Text>
-        }
-        className='flex-1 p-5'
+        renderSectionHeader={({ section: { title } }) => (
+          <Text className="text-xl text-white font-heading mt-8 mb-3">{title}</Text>
+        )}
+        className="flex-1 p-5"
         showsVerticalScrollIndicator={false}
         contentContainerStyle={{ paddingBottom: 100 }}
       />
-
     </View>
-  )
+  );
 }
