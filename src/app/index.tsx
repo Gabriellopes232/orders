aa68ccac7be378d585ddcdfa2557db1a9867920c
gabriellopes232 <gabriel.lopes.nascimento.dev@gmail.com>
2024-02-06 19:25:49 -0300

    first commit

---
+++ src/app/index.tsx
@@ -0,0 +1,67 @@
+import { CategoryButton } from '@/components/category-button'
+import { Header } from '@/components/header'
+import { View, FlatList, SectionList, Text } from 'react-native'
+import { CATEGORIES, MENU } from '@/utils/data/products'
+import { useState, useRef } from 'react'
+import { Product } from '@/components/product'
+
+export default function Home() {
+  const [category, setCategory] = useState(CATEGORIES[0])
+
+  const sectionListRef = useRef<SectionList>(null)
+
+  function handleCategorySelect(selectedCategory: string) {
+    setCategory(selectedCategory)
+
+    const sectionIndex = CATEGORIES.findIndex((category) => category === selectedCategory)
+
+    if (sectionListRef.current) {
+      sectionListRef.current.scrollToLocation({
+        animated: true,
+        sectionIndex,
+        itemIndex: 0
+      })
+    }
+  }
+
+  return (
+    <View className='flex-1 pt-8'>
+      <Header title='Faça o seu pedido' />
+
+      <FlatList
+        data={CATEGORIES}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => (
+          <CategoryButton
+            title={item}
+            isSelected={item === category}
+            onPress={() => handleCategorySelect(item)}
+          />
+        )}
+        horizontal
+        className='max-h-10 mt-5'
+        showsHorizontalScrollIndicator={false}
+        contentContainerStyle={{ gap: 12, paddingHorizontal: 20 }}
+      />
+
+      <SectionList
+        ref={sectionListRef}
+        sections={MENU}
+        keyExtractor={(item) => item.id}
+        stickySectionHeadersEnabled={false}
+        renderItem={({ item }) => (
+          <Product data={item} />
+        )}
+        renderSectionHeader={({ section: { title } }) =>
+          <Text className='text-xl text-white font-heading mt-8 mb-3'>
+            {title}
+          </Text>
+        }
+        className='flex-1 p-5'
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 100 }}
+      />
+
+    </View>
+  )
+}
